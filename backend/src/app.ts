f035095cb892b89f8ffb7d1f4310207aee42133d
nakim526 <knabilhakin@gmail.com>
2026-02-14 19:26:21 +0800

    sedikit perubahan

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,7 +5,9 @@ import path from "path";
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../../public')));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
 
 app.use(express.json());
 app.use('/api', routes);
