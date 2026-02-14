b0e5f70d0432199ee8758ce18c9fb740195f7c9f
nakim526 <knabilhakin@gmail.com>
2026-02-14 19:33:19 +0800

    coba sedikit

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,16 +1,18 @@
-import express from 'express';
-import routes from './routes';
-import { errorMiddleware } from './middlewares/error.middleware';
+import express from "express";
+import routes from "./routes";
+import { errorMiddleware } from "./middlewares/error.middleware";
 import path from "path";
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/index.html'));
+app.get("/", (req, res) => {
+  const filePath = path.join(__dirname, "../../public/index.html");
+  console.log("FILE PATH:", filePath);
+  res.sendFile(filePath);
 });
 
 app.use(express.json());
-app.use('/api', routes);
+app.use("/api", routes);
 app.use(errorMiddleware);
 
 export default app;
