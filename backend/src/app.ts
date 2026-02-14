b00095d0b175465a5e7b8e9455481d910ef11b48
nakim526 <knabilhakin@gmail.com>
2026-02-14 18:28:14 +0800

    perbaikan test login html

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import routes from './routes';
 import { errorMiddleware } from './middlewares/error.middleware';
+import path from "path";
 
 const app = express();
 
-app.get('/test-login', (req, res) => res.sendFile('./index.html'));
+app.use(express.static(path.join(__dirname, '../../public')));
 
 app.use(express.json());
 app.use('/api', routes);
