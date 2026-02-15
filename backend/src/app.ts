d4dda3ac4dd311d4679df2e14e86da7e70e82ac9
nakim526 <knabilhakin@gmail.com>
2026-02-16 00:26:05 +0800

    fixing debug log error

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,12 +11,4 @@ app.use(express.json());
 app.use("/api", routes);
 app.use(errorMiddleware);
 
-app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-  console.error(err);
-
-  res.status(err.statusCode || 500).json({
-    message: err.message || "Internal Server Error",
-  });
-});
-
 export default app;
--- backend/src/middlewares/error.middleware.ts
+++ backend/src/middlewares/error.middleware.ts
@@ -1,11 +1,16 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction } from "express";
+import { logger } from "../utils/logger";
 
 export const errorMiddleware = (
   err: any,
   _req: Request,
   res: Response,
-  _next: NextFunction
+  _next: NextFunction,
 ) => {
   console.error(err);
-  res.status(500).json({ message: 'Internal server error' });
+  logger.error(err);
+
+  res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+  });
 };
